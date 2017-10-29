68aef456a42cfec46f4025647e2bcaa0fa6580d8
Erick Diaz <erick.diaz@xoom.com>
2017-10-28 22:09:27 -0600

    completed dev script

--- extractNewData.py
+++ extractNewData.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import csv
 
 def validatePathName(pathName):
 	if(pathName.endswith('/')):
@@ -9,8 +9,11 @@ def validatePathName(pathName):
 		return pathName + '/'
 
 
-devInputPath = '/home/erick/Documents/kaggle/edgar_philip_love_poe/corpus'
-devOutputPath = '/home/erick/Documents/kaggle/kaggle_spooky_author_identification/'
+#devInputPath = '/home/erick/Documents/kaggle/edgar_philip_love_poe/corpus'
+#devOutputPath = '/home/erick/Documents/kaggle/kaggle_spooky_author_identification/'
+devInputPath = '/Users/erickdiaz/Documents/kaggle_spooky_author_identification/edgar_philip_love_poe/corpus'
+devOutputPath = '/Users/erickdiaz/Documents/kaggle_spooky_author_identification/kaggle_spooky_author_identification/'
+outputFileName = 'newData.csv'
 
 sourcePath = validatePathName(devInputPath if ( len(sys.argv) < 2 ) else sys.argv[1])
 outputPath = validatePathName(devOutputPath if (len(sys.argv) < 3) else sys.argv[2])
@@ -23,12 +26,26 @@ print('Output path:',outputPath)
 dirs = next(os.walk(sourcePath))[1]
 
 for path in dirs:
-	currentReadPath = validatePathName(sourcePath+path)
-	print('-->',path)
+	currentReadPath = validatePathName(sourcePath + path)
+	author = authorsDict[path]
+	print('-->' + path)
 	files = os.listdir(currentReadPath)
+
 	for file in files[:5]:
 		if (file.endswith('txt')):
-			print(file)
+			print('|----------- ' + file)
+
 			with open((currentReadPath+file), 'r') as f:
 				text=f.read()
-			print(text.split('.')[:4])	
+			newData = text.split('.')[:4]
+			outFile = outputPath + outputFileName
+			print(outFile)
+
+			with open(outFile, 'a') as csvfile:
+			    fieldnames = ['author', 'text']
+			    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+
+			    for text in newData:
+			    	print(author + '###########' + text)
+			    	writer.writerow({'author': author, 'text': text})
+
